739ae2da3451ec5889fabba038712d2cb8c4eee4
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-04-02 15:54:29 +0530

    Version API

--- server/src/server.js
+++ server/src/server.js
@@ -1,25 +1,15 @@
-const http = require("http");
-const mongoose = require("mongoose");
+const http = require('http');
 
-const app = require("./app");
-const { loadPlanetsData } = require("./models/planets.model");
+const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.model');
+const { mongoConnect } = require('./services/mongo');
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URL =
-  "mongodb+srv://nasa-db:XXXXXXXX@nasacluster.hkvrj.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB Connection Ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
